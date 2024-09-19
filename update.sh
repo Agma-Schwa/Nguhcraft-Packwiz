fdadec1d1a754551969bd3de47ee599e176661b0
Sirraide <aeternalmail@gmail.com>
2024-09-19 20:47:45 +0200

    Update Nguhcraft

--- update.sh
+++ update.sh
@@ -11,5 +11,11 @@ if test $# -ne 1; then
     die "Usage: ./update.sh <version>"
 fi
 
+# Copy the JAR.
 cp ../Nguhcraft-Fabric/build/libs/Nguhcraft-$1.jar mods/Nguhcraft.jar
+
+# Bump the version.
+sed -i -E "s/version = \".+\"/version = \"$1\"/" pack.toml
+
+# Update the index.
 packwiz refresh
